86a6d3c934788b762abbaa8a65fa84d727d6c78a
ViTuratti <125040095+ViTuratti@users.noreply.github.com>
2023-08-11 09:40:16 -0300

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return ( //JSX
+    <nav className="bg-slate-500 p-2 flex gap-3 items-end">
+      <h1 className="text-4xl text-zinc-100 font-bold uppercase">FIAP Filmes</h1>
+      <ul>
+        <li>
+          <a href="#">Favoritos</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
