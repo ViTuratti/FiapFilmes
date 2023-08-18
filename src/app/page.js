a617efa26e6d06818f576db268bcf25b5ea250e2
ViTuratti <125040095+ViTuratti@users.noreply.github.com>
2023-08-18 09:11:57 -0300

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
+import Titulo from "@/components/Titulo";
 
 export default function Home() {
   return ( //JSX
+  <>
     <nav className="bg-slate-500 p-2 flex gap-3 items-end">
       <h1 className="text-4xl text-zinc-100 font-bold uppercase">FIAP Filmes</h1>
       <ul>
@@ -9,5 +11,8 @@ export default function Home() {
         </li>
       </ul>
     </nav>
+    <Titulo texto="Em alta"/>
+    <Titulo texto="Lançamentos"/>
+    </>
   )
 }
