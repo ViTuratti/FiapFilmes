9634abd4cb31d512d866fb955f4c626bd84c3d08
ViTuratti <125040095+ViTuratti@users.noreply.github.com>
2023-08-25 09:36:52 -0300

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,28 @@
+import CardFilme from "@/components/CardFilme";
 import Titulo from "@/components/Titulo";
 
 export default function Home() {
+
+  const filmes= [
+    {
+      titulo: "Sobrenatural: A Porta Vermelha",
+      nota: 9.5,
+      poster: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/6lp4uDxLqLEw1CzW1SUOYJ3zdKD.jpg"
+    },
+    {
+      titulo: "Invocação do Mal 3: A Ordem do Demônio",
+      nota: 9.8,
+      poster: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/tLmaOXT9rMb17ISQDURKQIu3Okk.jpg"
+    },
+    {
+      titulo: "O Ritual",
+      nota: 7.5,
+      poster: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/yFRTz69BvtqZa7VjkyNGehjVb6p.jpg"
+    }
+  ] 
+
+
+
   return ( //JSX
   <>
     <nav className="bg-slate-500 p-2 flex gap-3 items-end">
@@ -12,6 +34,11 @@ export default function Home() {
       </ul>
     </nav>
     <Titulo>Em alta</Titulo>
+    <section className="flex flex-wrap gap-2">
+      {filmes.map( filme => <CardFilme filme={filme} />)}
+    </section>
+
+
     <Titulo>Lançamento</Titulo>
     </>
   )
