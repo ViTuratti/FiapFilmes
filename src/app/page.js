fc5c153468bc6e9369ccee113628f1fe2208827e
ViTuratti <125040095+ViTuratti@users.noreply.github.com>
2023-09-15 09:41:03 -0300

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,24 @@
 import CardFilme from "@/components/CardFilme";
 import Titulo from "@/components/Titulo";
 
-export default function Home() {
+async function carregarDados(){
+  const url = "https://api.themoviedb.org/3/trending/movie/week?api_key=1e922667481ab207d642450b0efb461e&language=pt-br"
+  const response = await fetch(url)
+  const json = await response.json()
+  return json.results
+}
 
-  const filmes= [
-    {
-      id: 1,
-      titulo: "Sobrenatural: A Porta Vermelha",
-      nota: 9.5,
-      poster: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/6lp4uDxLqLEw1CzW1SUOYJ3zdKD.jpg"
-    },
-    {
-      id: 2,
-      titulo: "Invocação do Mal 3: A Ordem do Demônio",
-      nota: 9.8,
-      poster: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/tLmaOXT9rMb17ISQDURKQIu3Okk.jpg"
-    },
-    {
-      id: 3,
-      titulo: "O Ritual",
-      nota: 7.5,
-      poster: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/yFRTz69BvtqZa7VjkyNGehjVb6p.jpg"
-    }
-  ] 
+export default async function Home() {
 
-
+  const filmes= await carregarDados()
 
   return ( //JSX
   <>
-    <nav className="bg-slate-500 p-2 flex gap-3 items-end">
-      <h1 className="text-4xl text-zinc-100 font-bold uppercase">FIAP Filmes</h1>
-      <ul>
-        <li>
-          <a href="#">Favoritos</a>
-        </li>
-      </ul>
+    <nav className="bg-slate-500 p-2 flex gap-4 items-end">
+      <h1 className="text-3xl text-zinc-100 font-bold uppercase">FIAP Filmes</h1>
+      <a href="/favoritos">Favoritos</a>
+
+
     </nav>
     <Titulo>Em alta</Titulo>
     <section className="flex flex-wrap gap-2">
